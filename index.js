2765474c945be408d67ed1d341b9980ec9da5b93
James Orcutt <ej.orcutt@gmail.com>
2018-08-20 22:47:19 -0400

    Adds stupid body parser

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const path = require('path');
+const bodyparser = require('body-parser');
+
 const PORT = process.env.PORT || 5000;
+
 // Currently config is empty, so use default private testing key
 const config = require('./config.json');
 const stripe = require('stripe')(config.secret_key || 'sk_test_BQokikJOvBiI2HlWgH4olfQ2');
 
 express().use(express.static(path.join(__dirname, 'public')))
+  .use(bodyparser.json())
+  .use(bodyparser.urlencoded({extended: true}))
   .get('/', (req, resp) => {
     resp.json({'hi': 'there'});
   }).post('/process_single_payment', (req, resp) => {
